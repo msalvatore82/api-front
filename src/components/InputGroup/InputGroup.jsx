e5fbf0fdc3b279f212f0f0f8a4bc09ba01945eaa
JonaTheBridge <jonathan@thebridgeschool.es>
2023-07-15 13:37:10 +0200

    improve InputGroup component styles

--- src/components/InputGroup/InputGroup.jsx
+++ src/components/InputGroup/InputGroup.jsx
@@ -2,9 +2,9 @@ import './InputGroup.css';
 
 function InputGroup(props) {
   return (
-    <div className='inputGroup'>
-      <label htmlFor={ props.input.id }> { props.label } </label>
-      <input
+    <div className='inputGroup row'>
+      <label className='col-12 col-md-4 col-lg-3' htmlFor={ props.input.id }> { props.label } </label>
+      <input className='col-12 col-md-8 col-lg-9'
         onChange={ props.input.onChange}
         id={ props.input.id }
         placeholder={ props.input.placeholder }

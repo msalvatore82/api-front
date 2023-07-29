d6a9a06e27e2f6e879fee8a1ae38309f24bad4bd
JonaTheBridge <jonathan@thebridgeschool.es>
2023-07-29 12:15:41 +0200

    improve context requesting loggedUser data at render

--- src/contexts/loggedUserContext.jsx
+++ src/contexts/loggedUserContext.jsx
@@ -1,9 +1,19 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useState, useEffect } from 'react';
+import { useApi } from '../hooks/useApi.js';
 
 const LoggedUserContext = createContext();
 
 export function LoggedUserProvider({ children }) {
+  const { request, data } = useApi();
   const [loggedUser, setLoggedUser] = useState({});
+
+  useEffect(() => {
+    localStorage.token && request({ route: 'users/getusernamebytoken' });
+  }, []);
+
+  useEffect(() => {
+    setLoggedUser({ ...data });
+  }, [data]);
 
   return (
     <LoggedUserContext.Provider value={{ loggedUser, setLoggedUser }}>
